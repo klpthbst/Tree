d76996616b02b2b37a6ffe38664855fdc99b7389
klpthbst <klepathebest@gmail.com>
2018-11-04 19:41:07 +0200

    add createTree function functions height, computeHeight - worked not a right way

--- src/TreeHeight.java
+++ src/TreeHeight.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 public class TreeHeight {
     int n;
     int parent[];
+    Node tree;
 
     void read() throws IOException {
         FastScanner in = new FastScanner();
@@ -11,30 +12,41 @@ public class TreeHeight {
         for (int i = 0; i < n; i++) {
             parent[i] = in.nextInt();
         }
+        tree = createTree(parent, n);
     }
 
-    int computeHeight() {
-        int maxHeight = 0;
-        Node nodes[] = new Node[n];
+    Node createTree(int arr[], int size) {
+
+        Node nodes[] = new Node[size];
         int root = -1;
-        for (int i = 0; i < n; i++) {
-            nodes[i] = new Node(parent[i]);
+        for (int i = 0; i < size; i++) {
+            nodes[i] = new Node(arr[i]);
         }
 
         for (int i = 0; i < nodes.length; i++) {
             if(nodes[i].getData() == -1) {
-                //its root
                 root = i;
-            } else if (nodes[i].getData() == root) {
-                nodes[root].addChild(nodes[i]);
-                System.out.println("root!");
             } else {
                 nodes[nodes[i].getData()].addChild(nodes[i]);
-                System.out.println("Not root!");
-
             }
         }
+        return nodes[root];
+    }
 
+    int height(Node tree) {
+        if(tree == null)
+            return 0;
+        return 1 + height(tree.getLeft());
+
+    }
+    int computeHeight() {
+        int maxHeight = 0;
+        int height;
+        while (tree != null) {
+            height = height(tree);
+            maxHeight = height > maxHeight ? height : maxHeight;
+            tree = tree.getRight();
+        }
         return maxHeight;
     }
 }
